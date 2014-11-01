fcb8dd8a457ee1dc1e9253a65e5c6ed6142bdf43
Tim Hart <tjhart@icloud.com>
2014-10-31 20:13:50 -0500

    fetch existing designs at once to save on traffic and time

--- specs/Brocfile.js
+++ specs/Brocfile.js
@@ -2,6 +2,8 @@ var couchVersioner = require('../index');
 
 module.exports = couchVersioner('design/', {
   url: 'http://localhost:5984/test',
-  username: 'tjhart',
-  password: 'icthus'
+  username: 'test',
+  password: 'test'
+
+
 });
